289b162afdb1df89821f9b6e2dc3ad4e92e623bf
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-23 16:13:15 -0600

    add HomePage component with multiple headings and a Button

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,3 +1,14 @@
+import { Button } from '@/components/ui/button';
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      <h1 className="font-montserrat">Hola mundo</h1>
+      <h1 className="font-montserrat font-thin">Hola mundo</h1>
+      <h1 className="font-montserrat font-normal">Hola mundo</h1>
+      <h1 className="font-montserrat font-bold">Hola mundo</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 };
