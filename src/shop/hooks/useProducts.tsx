d89ecc80f3ef659c63ca1688b79f6b49e61eaaaa
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-24 22:37:32 -0600

    feat: enhance useProducts hook to support dynamic product retrieval with pagination and filters

--- src/shop/hooks/useProducts.tsx
+++ src/shop/hooks/useProducts.tsx
@@ -1,11 +1,29 @@
 import { useQuery } from '@tanstack/react-query';
 import { getProductsAction } from '../actions/get-products.action';
+import { useParams, useSearchParams } from 'react-router';
 
 export const useProducts = () => {
   // TODO: logica pendiente
 
+  const [searchParams] = useSearchParams();
+  const { gender } = useParams();
+
+  const limit = searchParams.get('limit') || 9;
+  const page = searchParams.get('page') || 1;
+
+  const sizes = searchParams.get('sizes') || undefined;
+
+  const offset = (Number(page) - 1) * Number(limit);
+
   return useQuery({
-    queryKey: ['products'],
-    queryFn: getProductsAction,
+    queryKey: ['products', { limit, offset, sizes, gender }],
+    queryFn: () =>
+      getProductsAction({
+        limit: isNaN(+limit) ? 9 : Number(limit),
+        offset: isNaN(offset) ? 0 : offset,
+        sizes,
+        gender,
+      }),
+    staleTime: 1000 * 60 * 5, // 5 minutes
   });
 };
