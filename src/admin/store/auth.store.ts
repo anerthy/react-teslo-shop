8ad050134f275c84f1cdb87095ba2768667cade0
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-25 00:16:10 -0600

    feat: implement useAuthStore for user authentication management

---
+++ src/admin/store/auth.store.ts
@@ -0,0 +1,44 @@
+import type { User } from '@/interfaces/user.interface';
+import { create } from 'zustand';
+import { loginAction } from '../actions/login.action';
+
+type AuthStatus = 'authenticated' | 'unauthenticated' | 'checking';
+
+type AuthState = {
+  // Properties
+  user: User | null;
+  token: string | null;
+  authStatus: AuthStatus;
+  // Getters
+
+  // Actions
+  login(email: string, password: string): Promise<boolean>;
+  logout(): void;
+};
+
+export const useAuthStore = create<AuthState>()((set) => ({
+  user: null,
+  token: null,
+  authStatus: 'checking',
+
+  login: async (email: string, password: string): Promise<boolean> => {
+    try {
+      const data = await loginAction(email, password);
+      localStorage.setItem('token', data.token);
+
+      set({ user: data.user, token: data.token });
+      return true;
+    } catch (error) {
+      // console.log(error);
+
+      localStorage.removeItem('token');
+      set({ user: null, token: null });
+      return false;
+    }
+  },
+
+  logout: () => {
+    localStorage.removeItem('token');
+    set({ user: null, token: null });
+  },
+}));
