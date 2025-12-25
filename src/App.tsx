cf1b49a374088ab7c304247e5c5d27c74fa59f36
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-24 18:41:33 -0600

    feat: integrate React Query for data fetching with QueryClient and ReactQueryDevtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { RouterProvider } from 'react-router';
 import { appRouter } from './app.router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-// TesloShopApp
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={appRouter} />;
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={appRouter} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
