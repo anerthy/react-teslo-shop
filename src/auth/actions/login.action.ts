87ac6d2f01234c369539ea234df64b0786600a2b
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-25 12:41:11 -0600

    feat: remove unused loginAction from admin actions

--- src/auth/actions/login.action.ts
+++ src/auth/actions/login.action.ts
@@ -1,5 +1,5 @@
 import { tesloApi } from '@/api/teslo-api';
-import type { AuthResponse } from '../interfaces/auth.response';
+import type { AuthResponse } from '../../admin/interfaces/auth.response';
 
 export const loginAction = async (
   email: string,
