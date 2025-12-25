4fbdffad9c6e46cd8f98e61ab2ba26c6ec7316e8
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-24 23:35:11 -0600

    feat: add Toaster component for improved user notifications

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router';
 import { appRouter } from './app.router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'sonner';
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        <Toaster />
         <RouterProvider router={appRouter} />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
