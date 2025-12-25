b3d89a3716e83c6a0c4c18a859f3f48eb393fcc5
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-24 22:53:28 -0600

    feat: enhance useProducts hook to support price range filtering and query parameters

--- src/shop/hooks/useProducts.tsx
+++ src/shop/hooks/useProducts.tsx
@@ -11,18 +11,48 @@ export const useProducts = () => {
   const limit = searchParams.get('limit') || 9;
   const page = searchParams.get('page') || 1;
 
+  const price = searchParams.get('price') || 'any';
+
   const sizes = searchParams.get('sizes') || undefined;
+
+  const query = searchParams.get('query') || undefined;
 
   const offset = (Number(page) - 1) * Number(limit);
 
+  let minPrice = undefined;
+  let maxPrice = undefined;
+
+  switch (price) {
+    case 'any':
+      break;
+    case '0-50':
+      minPrice = 0;
+      maxPrice = 50;
+      break;
+    case '50-100':
+      minPrice = 50;
+      maxPrice = 100;
+      break;
+    case '100-200':
+      minPrice = 100;
+      maxPrice = 200;
+      break;
+    case '200+':
+      minPrice = 200;
+      break;
+  }
+
   return useQuery({
-    queryKey: ['products', { limit, offset, sizes, gender }],
+    queryKey: ['products', { limit, offset, sizes, gender, price, query }],
     queryFn: () =>
       getProductsAction({
         limit: isNaN(+limit) ? 9 : Number(limit),
         offset: isNaN(offset) ? 0 : offset,
         sizes,
         gender,
+        minPrice,
+        maxPrice,
+        query,
       }),
     staleTime: 1000 * 60 * 5, // 5 minutes
   });
