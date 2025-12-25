3a150b74d99af8ad4a02b6ff9e4f9cb0abf23fa4
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-25 11:58:51 -0600

    feat: enhance auth store with checkAuthStatus action and improve admin role validation

--- src/admin/store/auth.store.ts
+++ src/admin/store/auth.store.ts
@@ -1,6 +1,7 @@
 import type { User } from '@/interfaces/user.interface';
 import { create } from 'zustand';
 import { loginAction } from '../actions/login.action';
+import { checkAuthAction } from '../actions/check-auth.action';
 
 type AuthStatus = 'authenticated' | 'unauthenticated' | 'checking';
 
@@ -10,35 +11,53 @@ type AuthState = {
   token: string | null;
   authStatus: AuthStatus;
   // Getters
-
+  isAdmin(): boolean;
   // Actions
   login(email: string, password: string): Promise<boolean>;
   logout(): void;
+  checkAuthStatus(): Promise<boolean>;
 };
 
-export const useAuthStore = create<AuthState>()((set) => ({
+export const useAuthStore = create<AuthState>()((set, get) => ({
   user: null,
   token: null,
   authStatus: 'checking',
+
+  isAdmin(): boolean {
+    const roles = get().user?.roles || []; // TODO: Revisar que valide bien
+    return roles.includes('admin');
+  },
 
   login: async (email: string, password: string): Promise<boolean> => {
     try {
       const data = await loginAction(email, password);
       localStorage.setItem('token', data.token);
 
-      set({ user: data.user, token: data.token });
+      set({ user: data.user, token: data.token, authStatus: 'authenticated' });
       return true;
     } catch (error) {
-      // console.log(error);
+      console.log(error);
 
       localStorage.removeItem('token');
-      set({ user: null, token: null });
+      set({ user: null, token: null, authStatus: 'unauthenticated' });
       return false;
     }
   },
 
   logout: () => {
     localStorage.removeItem('token');
-    set({ user: null, token: null });
+    set({ user: null, token: null, authStatus: 'unauthenticated' });
+  },
+
+  checkAuthStatus: async () => {
+    try {
+      const { user, token } = await checkAuthAction();
+      set({ user: user, token: token, authStatus: 'authenticated' });
+      return true;
+    } catch (error) {
+      console.log(error);
+      set({ user: undefined, token: undefined, authStatus: 'unauthenticated' });
+      return false;
+    }
   },
 }));
