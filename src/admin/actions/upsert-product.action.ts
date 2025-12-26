a70c06b9ff2c55c3732f19f2248844f4785d1d19
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-25 23:35:28 -0600

    feat: implement upsertProductAction for creating and updating products

---
+++ src/admin/actions/upsert-product.action.ts
@@ -0,0 +1,32 @@
+import { tesloApi } from '@/api/teslo-api';
+import type { Product } from '@/interfaces/product.interface';
+import { sleep } from '@/lib/sleep';
+
+export const upsertProductAction = async (
+  productLike: Partial<Product>
+): Promise<Product> => {
+  await sleep(1500);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { id, user, images = [], ...rest } = productLike;
+
+  const isCreating = !id || id === 'new';
+
+  rest.stock = Number(rest.stock || 0);
+  rest.price = Number(rest.price || 0);
+
+  const { data } = await tesloApi<Product>({
+    url: isCreating ? '/products' : `/products/${id}`,
+    method: isCreating ? 'POST' : 'PATCH',
+    data: rest,
+  });
+
+  return {
+    ...data,
+    images: data.images.map((img) =>
+      img.includes('http')
+        ? img
+        : `${import.meta.env.VITE_API_URL}/files/products/${img}`
+    ),
+  };
+};
