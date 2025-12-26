7a7277e41168892ff18b6626a834bdb562107b4f
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-25 23:35:57 -0600

    feat: add mutation for upserting products in useProduct hook

--- src/admin/hooks/useProduct.tsx
+++ src/admin/hooks/useProduct.tsx
@@ -1,7 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { getProductByIdAction } from '../actions/get-product-by-id.action';
+import { upsertProductAction } from '../actions/upsert-product.action';
+import type { Product } from '@/interfaces/product.interface';
 
 export const useProduct = (id: string) => {
+  const queryClient = useQueryClient();
+
   const query = useQuery({
     queryKey: ['product', { id }],
     queryFn: async () => getProductByIdAction(id),
@@ -11,8 +15,23 @@ export const useProduct = (id: string) => {
   });
 
   // TODO: Mutation
+  const mutation = useMutation({
+    mutationFn: upsertProductAction,
+    onSuccess: (data: Product) => {
+      // invalidar cache
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+      queryClient.invalidateQueries({ queryKey: ['product', { id: data.id }] });
+
+      // actualizar queryData
+      queryClient.setQueryData(['product', { id: data.id }], data);
+    },
+    onError: (error) => {
+      console.error('Error upserting product', error);
+    },
+  });
 
   return {
     ...query,
+    mutation,
   };
 };
