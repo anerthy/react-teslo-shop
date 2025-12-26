99fc7723d77eb6df6b4b87114de3704e620b76e0
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-26 17:30:02 -0600

    feat: enhance upsertProductAction to handle file uploads and image processing

--- src/admin/actions/upsert-product.action.ts
+++ src/admin/actions/upsert-product.action.ts
@@ -1,24 +1,41 @@
 import { tesloApi } from '@/api/teslo-api';
 import type { Product } from '@/interfaces/product.interface';
-// import { sleep } from '@/lib/sleep';
+import { sleep } from '@/lib/sleep';
 
 export const upsertProductAction = async (
-  productLike: Partial<Product>
+  productLike: Partial<Product> & { files?: File[] }
 ): Promise<Product> => {
-  // await sleep(1500);
+  await sleep(1500);
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { id, user, images = [], ...rest } = productLike;
+  const { id, user, images = [], files = [], ...rest } = productLike;
+
+  console.log({ files });
 
   const isCreating = !id || id === 'new';
 
   rest.stock = Number(rest.stock || 0);
   rest.price = Number(rest.price || 0);
 
+  // Preparar imagenes
+  if (files.length > 0) {
+    const newImageNames = await uploadFiles(files);
+    images.push(...newImageNames);
+  }
+
+  const imagesToSave = images.map((image) => {
+    if (image.includes('http')) return image.split('/').pop() || '';
+
+    return image;
+  });
+
   const { data } = await tesloApi<Product>({
     url: isCreating ? '/products' : `/products/${id}`,
     method: isCreating ? 'POST' : 'PATCH',
-    data: rest,
+    data: {
+      ...rest,
+      images: imagesToSave,
+    },
   });
 
   return {
@@ -29,4 +46,27 @@ export const upsertProductAction = async (
         : `${import.meta.env.VITE_API_URL}/files/products/${img}`
     ),
   };
+};
+
+export interface FileUploadResponse {
+  secureUrl: string;
+  fileName: string;
+}
+
+const uploadFiles = async (files: File[]) => {
+  const uploadPromises = files.map(async (file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const { data } = await tesloApi<FileUploadResponse>({
+      url: '/files/product',
+      method: 'POST',
+      data: formData,
+    });
+
+    return data.fileName;
+  });
+
+  const uploadedFileNames = await Promise.all(uploadPromises);
+  return uploadedFileNames;
 };
